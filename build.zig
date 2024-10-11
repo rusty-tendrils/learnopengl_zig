14d9efaad7dda58bd7e5a823293b61de25a19c10
sumadithya <sumadithya@gmail.com>
2024-10-11 15:18:50 +0530

    Hello Window

--- build.zig
+++ build.zig
@@ -1,14 +1,65 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) void {
-    const exe = b.addExecutable(.{
-        .name = "hello",
-        .link_libc = true,
-        .target = b.host,
-    });
-    exe.root_module.addCSourceFile(.{
-        .file = b.path("hello.c"),
-    });
+const flags = [_][]const u8{
+    "-Wall",
+    "-Wextra",
+    "-Werror=return-type",
+};
+const cflags = flags ++ [_][]const u8{
+    "-std=c99",
+};
 
-    b.installArtifact(exe);
+const cxxflags = cflags ++ [_][]const u8{
+    "-std=c++17", "-fno-exceptions",
+};
+
+pub fn build(b: *std.Build) void {
+    const targets = [_]Target{
+        .{ .name = "hello_window", .src = "src/1.getting_started/1.hello_window/main.cpp" },
+    };
+
+    for (targets) |target| {
+        target.build(b);
+    }
 }
+
+const Target = struct {
+    name: []const u8,
+    src: []const u8,
+
+    pub fn build(self: Target, b: *std.Build) void {
+        const exe = b.addExecutable(.{
+            .name = self.name,
+            .target = b.host,
+        });
+        exe.root_module.addCSourceFile(.{
+            .file = b.path(self.src),
+            .flags = &cxxflags,
+        });
+
+        // Includes
+        exe.addIncludePath(b.path("include"));
+
+        // Sources
+        exe.addCSourceFile(.{
+            .file = b.path("src/glad.c"),
+            .flags = &cflags,
+        });
+
+        // Libraries
+        exe.addLibraryPath(b.path("lib"));
+
+        exe.linkLibC();
+        exe.linkLibCpp();
+
+        exe.linkSystemLibrary("glfw3");
+        exe.linkSystemLibrary("GL");
+        exe.linkSystemLibrary("X11");
+        exe.linkSystemLibrary("pthread");
+        exe.linkSystemLibrary("Xrandr");
+        exe.linkSystemLibrary("Xi");
+        exe.linkSystemLibrary("dl");
+
+        b.installArtifact(exe);
+    }
+};
---
+++ src/1.getting_started/1.hello_window/main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#define GLFW_INCLUDE_NONE
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+int main()
+{
+    const int WIDTH = 800;
+    const int HEIGHT = 600;
+
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (gladLoadGLLoader((GLADloadproc)glfwGetProcAddress) == 0) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    glViewport(0, 0, WIDTH, HEIGHT);
+
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    while (glfwWindowShouldClose(window) == 0) {
+        processInput(window);
+
+
+        glClearColor(0.2F, 0.3F, 0.3F, 1.0F);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
+void framebuffer_size_callback([[maybe_unused]] GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+}
