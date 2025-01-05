a274ba1aa9b570cba35fe66e584108bb103fe5a3
rusty_tendrils <sumadithya@gmail.com>
2025-01-06 02:40:47 +0530

    Fixed flake

--- flake.nix
+++ flake.nix
@@ -1,32 +1,47 @@
 {
   description = "A Nix-flake-based development environment for opengl with zig and c++";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self , nixpkgs ,... }: let
-    system = "x86_64-linux";
-  in {
-    devShells."${system}".default = let
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-    in pkgs.mkShell {
-      packages = with pkgs; [
-        zig
-        zls
-        clang
-        clang-tools
-        glfw-wayland
-        #TODO What's pkg-config?
-        pkg-config
-      ];
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell.override
+          {
+            # Override stdenv in order to change compiler:
+            # stdenv = pkgs.clangStdenv;
+          }
+          {
+            packages = with pkgs; [
+              clang-tools
+              cmake
+              codespell
+              conan
+              cppcheck
+              doxygen
+              gtest
+              lcov
+              vcpkg
+              vcpkg-tool
+              zig
+              zls
+              clang
+              glfw-wayland
+              #TODO What's pkg-config?
+              pkg-config
+            ] ++ (if system == "aarch64-darwin" then [ ] else [ gdb ]);
 
-      shellHook = ''
-        echo "zig `${pkgs.zig}/bin/zig version`"
-        echo "clang `${pkgs.clang}/bin/clang --version`"
-      '';
+            shellHook = ''
+              echo "zig `${pkgs.zig}/bin/zig version`"
+              echo "clang `${pkgs.clang}/bin/clang --version`"
+            '';
+          };
+      });
     };
-  };
 }
